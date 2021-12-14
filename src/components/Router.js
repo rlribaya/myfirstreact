e71864c62c79f3e2e61e6c43a2c73627d0bd41f0
rlribaya <79568279+rlribaya@users.noreply.github.com>
2021-12-14 19:11:51 +0800

    Fixed URLs

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,7 +9,7 @@ const Router = () => {
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="myfirstreact/" element={<Home/>}/>
                     <Route exact path="/employees" element={<Employee/>}/>
                     <Route exact path="/add" element={<AddEmployee/>}/>
                     <Route exact path="/edit/:employeeId" element={<AddEmployee/>}/>
