1562576ff143878044163e616e3ec1fe480cd950
rlribaya <79568279+rlribaya@users.noreply.github.com>
2021-12-14 19:24:37 +0800

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route, HashRouter } from "react-router-dom"
 import AddEmployee from "./AddEmployee"
 import Employee from "./Employee"
 import Home from "./Home"
@@ -7,7 +7,7 @@ import PageNotFound from "./PageNotFound"
 const Router = () => {
     return(
         <div>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route exact path="/" element={<Home/>}/>
                     <Route exact path="/employees" element={<Employee/>}/>
@@ -15,7 +15,7 @@ const Router = () => {
                     <Route exact path="/edit/:employeeId" element={<AddEmployee/>}/>
                     <Route exact path="*" element={<PageNotFound/>}/>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     )
 }
